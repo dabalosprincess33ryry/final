616f607dbc5884649dfecb14b256160925748e0c
Ryan Fallore <falloreryan649@gmail.com>
2026-01-09 05:44:57 +0800

    Update database settings for Render

--- FinGo/settings.py
+++ FinGo/settings.py
@@ -201,12 +201,22 @@ WSGI_APPLICATION = 'FinGo.wsgi.application'
 # -----------------------------
 # DATABASE
 # -----------------------------
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
+#     }
+# }
+
+import os
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL')
+    )
 }
+
 
 # -----------------------------
 # PASSWORD VALIDATION
