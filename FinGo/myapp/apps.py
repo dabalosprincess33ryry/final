9b39f4d47d45942097c6daa93793ae8ba6db6e4e
dabalosprincess33ryry <dabalosprincess33@gmail.com>
2026-01-10 23:14:23 +0800

    Update apps.pybahala na

--- FinGo/myapp/apps.py
+++ FinGo/myapp/apps.py
@@ -1,17 +1,16 @@
 from django.apps import AppConfig
-
+from django.contrib.auth import get_user_model
 
 class MyappConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'myapp'
     
     def ready(self):
-        # Keep your signals import
         import myapp.signals  
 
-        # Create superuser only if it doesn't exist
         User = get_user_model()
         username = "admin"
+
         if not User.objects.filter(username=username).exists():
             User.objects.create_superuser(
                 username=username,
