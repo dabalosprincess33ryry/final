df9e952acbf21b4ba95acc5cb3a54d421a6160fc
Ryan Fallore <falloreryan649@gmail.com>
2026-01-10 20:29:35 +0800

    Initial commit

--- FinGo/settings.py
+++ FinGo/settings.py
@@ -199,25 +199,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'FinGo.wsgi.application'
 
-# -----------------------------
-# DATABASE
-# -----------------------------
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': BASE_DIR / 'db.sqlite3',
-#     }
-# }
-
-import os
-import dj_database_url
 
 DATABASES = {
-    'default': dj_database_url.config(
-        default=os.environ.get('DATABASE_URL')
-    )
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
-
 
 # -----------------------------
 # PASSWORD VALIDATION
